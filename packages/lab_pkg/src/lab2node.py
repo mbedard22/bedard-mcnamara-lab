5bbde52f294d475e966d0f2a087ada30ddd1bacd
mcnliamj <William_McNamara@student.uml.edu>
2022-11-15 17:38:42 -0500

    11-15-22

--- packages/lab_pkg/src/lab2node.py
+++ packages/lab_pkg/src/lab2node.py
@@ -13,31 +13,31 @@ class Director:
         self.pub = rospy.Publisher('/lane_controller_node/car_cmd',Twist2DStamped,queue_size=10)
         rospy.Subscriber('/fsm_node/mode', FSMState, self.mode_trigger)
 
-        self.running = FALSE
+        self.running = False
         
         
     def mode_trigger(self,msg):
                   
-        if msg.state == "LANE_FOLLOWING" and self.running == FALSE:
-            self.running = TRUE
+        if msg.state == "LANE_FOLLOWING" and self.running == False:
+            self.running = True
             # start driving
              
-        else if msg.state == "LANE_FOLLOWING" and self.running == TRUE:
-            break;
+        elif msg.state == "LANE_FOLLOWING" and self.running == True:
+            pass
             
-        else if msg.state == "NORMAL_JOYSTICK_CONTROL":
-            self.running = FALSE
+        elif msg.state == "NORMAL_JOYSTICK_CONTROL":
+            self.running = False
           
        
-    def drive_straight(self, msg):
-        if self.running == TRUE:
+    def drive_straight(self):
+        if self.running == True:
             cmd = Twist2DStamped()
             cmd.v = 2
             cmd.omega = 0
           
             rospy.pub.publish(cmd)
-    def turn(self, msg):
-        if self.running == TRUE:
+    def turn(self):
+        if self.running == True:
             cmd = Twist2DStamped()
             cmd.v = 0
             cmd.omega = 5
@@ -50,7 +50,7 @@ if __name__=='__main__':
         d = Director()
         rate = rospy.Rate(10)
         while not rospy.is_shutdown():
-            for i in range[3]:
+            for i in range(3):
                
                d.drive_straight()
                rate.sleep()
