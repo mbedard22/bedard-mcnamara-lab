01e54a27da37751f5841fb2e461827af0af75f45
mcnliamj <William_McNamara@student.uml.edu>
2022-12-07 10:33:33 -0500

    12-7

--- packages/lab_pkg/src/lab4node.py
+++ packages/lab_pkg/src/lab4node.py
@@ -7,12 +7,34 @@ from duckietown_msgs.msg import Twist2DStamped
 class PID_controller:
     def __init__(self):
         rospy.Subscriber("apriltag_detector_node/detections", AprilTagDetectionArray, self.callback)
+        rospy.Subscriber('fsm_node/mode', FSMState, self.mode_trigger)
+
         self.pub=rospy.Publisher("lane_controller_node/car_cmd", Twist2DStamped, queue_size = 10)
+        
+        self.running = False
+        
+        
+    def mode_trigger(self,msg):
+                  
+        if msg.state == "LANE_FOLLOWING" and self.running == False:
+            self.running = True
+            # start driving
+            
+        elif msg.state == "LANE_FOLLOWING" and self.running == True:
+            pass
+            
+        elif msg.state == "NORMAL_JOYSTICK_CONTROL":
+            self.running = False
+        
 
     def callback(self, msg):
-        cmd = Twist2DStamped()
-        cmd.v = msg.detections[0].transform.translation.x
-        self.pub.publish(cmd)
+        if self.running == True:
+            cmd = Twist2DStamped()
+            
+            #PID for angular
+            
+            cmd.omega = msg.detections[0].transform.translation.x
+            self.pub.publish(cmd)
 
 if __name__=='__main__':
     rospy.init_node("lab4node.py", anonymous = True)
