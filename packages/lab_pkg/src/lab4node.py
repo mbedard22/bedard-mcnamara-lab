7eeba1ace35d08e7922a6a15b899cd16c944d155
mcnliamj <William_McNamara@student.uml.edu>
2022-12-13 11:44:51 -0500

    12-13

--- packages/lab_pkg/src/lab4node.py
+++ packages/lab_pkg/src/lab4node.py
@@ -1,4 +1,4 @@
- #!/usr/bin/env python3
+#!/usr/bin/env python3
 
 import rospy
 from duckietown_msgs.msg import AprilTagDetectionArray
@@ -7,37 +7,43 @@ from duckietown_msgs.msg import Twist2DStamped
 class PID_controller:
     def __init__(self):
         rospy.Subscriber("apriltag_detector_node/detections", AprilTagDetectionArray, self.callback)
-        rospy.Subscriber('fsm_node/mode', FSMState, self.mode_trigger)
 
-        self.pub=rospy.Publisher("lane_controller_node/car_cmd", Twist2DStamped, queue_size = 10)
-        
-        self.running = False
-        
-        
-    def mode_trigger(self,msg):
-                  
-        if msg.state == "LANE_FOLLOWING" and self.running == False:
-            self.running = True
-            # start driving
-            
-        elif msg.state == "LANE_FOLLOWING" and self.running == True:
-            pass
-            
-        elif msg.state == "NORMAL_JOYSTICK_CONTROL":
-            self.running = False
-        
+        self.pub=rospy.Publisher("car_cmd_switch_node/cmd", Twist2DStamped, queue_size = 10)
+
+        self.prev_error_A = 0
+        self.cum_error_A = 0
+        self.elapsedTime = 0.03 ##need to find actual timestep
+
+
+
 
     def callback(self, msg):
-        if self.running == True:
             cmd = Twist2DStamped()
-            
+
             #PID for angular
-            
-            cmd.omega = msg.detections[0].transform.translation.x
+            self.error_A = msg.detections[0].transform.translation.x
+
+            K_p_a = 2
+            K_i_a = 0.01
+            K_d_a = 0.25
+
+            #determine error between target and current position
+            #need to determine elapsed time for integral and derivative 
+
+            self.cum_error_A += self.error_A * self.elapsedTime
+            rate_error_A = (self.error_A-self.prev_error_A)/self.elapsedTime
+
+            cmd.omega = (K_p_a * self.error_A) + (K_i_a * self.cum_error_A) + (K_d_a * rate_error_A)
+            cmd.omega = -cmd.omega
             self.pub.publish(cmd)
+            rospy.logwarn(cmd.omega)
+
+            self.prev_error_A = msg.detections[0].transform.translation.x
+
 
 if __name__=='__main__':
     rospy.init_node("lab4node.py", anonymous = True)
     p = PID_controller()
     rospy.spin()
+
 
